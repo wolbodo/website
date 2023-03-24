aaebe5ff745fb4d4de36959647c97dd7d74887b9
Dexter <dexter@dxlb.nl>
2023-03-24 20:50:30 +0100

    No need for tokens

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,12 +1,7 @@
 import type { PageLoad } from './$types';
-import { env } from '$env/dynamic/public';
 
 export const load = (async ({ fetch }) => {
-    const res = await fetch('https://cms.wolbodo.nl/api/posts', {
-        headers: {
-            Authorization: `Bearer ${env.PUBLIC_API_KEY}`
-        },
-    })
+    const res = await fetch('https://cms.wolbodo.nl/api/posts')
 
     return {
         posts: await res.json()
