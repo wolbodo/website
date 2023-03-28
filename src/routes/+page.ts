124a937fecf60a3bdc05c787fb4aa7ea6d801f6a
Dexter <dexter@dxlb.nl>
2023-03-28 22:00:00 +0200

    Fix api loading

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,10 +1,18 @@
 import type { PageLoad } from './$types';
+import { env } from '$env/dynamic/public'
 
 export const load = (async ({ fetch }) => {
-    const res = await fetch('https://cms.wolbodo.nl/api/posts')
+    const res = await fetch('https://cms.wolbodo.nl/api/posts', {
+        headers: {
+            Authorization: `Bearer ${env.PUBLIC_API_KEY}`
+        }
+    })
+    console.log(res.ok, res.headers)
+    const posts = await res.json()
+    console.log(posts)
 
     return {
-        posts: await res.json()
+        posts
     };
 
 }) satisfies PageLoad;
